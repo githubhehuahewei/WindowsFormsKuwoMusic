d29c7750d1249ed072294e00b8d4c2cb4ab8868e
sunday258 <62322662+sunday258@users.noreply.github.com>
2020-03-22 11:24:07 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace WindowsFormsKuwoMusic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureplay = new System.Windows.Forms.PictureBox();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.butLoadLyricc = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureplay)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.panel1.Controls.Add(this.butLoadLyricc);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.pictureplay);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 365);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 85);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(278, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(73, 24);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "载入文件";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureplay
+            // 
+            this.pictureplay.BackColor = System.Drawing.Color.Transparent;
+            this.pictureplay.BackgroundImage = global::WindowsFormsKuwoMusic.Properties.Resources.play_circle_o;
+            this.pictureplay.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureplay.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureplay.Location = new System.Drawing.Point(357, 0);
+            this.pictureplay.Name = "pictureplay";
+            this.pictureplay.Size = new System.Drawing.Size(89, 85);
+            this.pictureplay.TabIndex = 0;
+            this.pictureplay.TabStop = false;
+            this.pictureplay.Click += new System.EventHandler(this.pictureplay_Click);
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(537, -12);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(251, 259);
+            this.axWindowsMediaPlayer1.TabIndex = 1;
+            this.axWindowsMediaPlayer1.Visible = false;
+            // 
+            // butLoadLyricc
+            // 
+            this.butLoadLyricc.Location = new System.Drawing.Point(452, 32);
+            this.butLoadLyricc.Name = "butLoadLyricc";
+            this.butLoadLyricc.Size = new System.Drawing.Size(75, 23);
+            this.butLoadLyricc.TabIndex = 2;
+            this.butLoadLyricc.Text = "载入歌词";
+            this.butLoadLyricc.UseVisualStyleBackColor = true;
+            this.butLoadLyricc.Click += new System.EventHandler(this.butLoadLyricc_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::WindowsFormsKuwoMusic.Properties.Resources.陈一发儿_童话镇1;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.panel1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureplay)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureplay;
+        private System.Windows.Forms.Button butLoadLyricc;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsKuwoMusic
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_(object sender, MouseEventArgs e)
+        {
+
+        }
+        /// <summary>
+        ///  移动窗口
+        /// </summary>
+        Point pointMouseDown;
+        Point pointMouseMove;
+        bool isMoving = false;//设置一开关，按住（mousedown）窗口时才打开
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            isMoving = true;
+            pointMouseDown = e.Location;
+        }
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isMoving)
+            {
+                pointMouseMove = e.Location;
+                this.Location = new Point(this.Location.X/*窗口原位置*/
+                    + (pointMouseMove.X - pointMouseDown.X/*变化的x坐标*/),
+                    this.Location.Y + (pointMouseMove.Y - pointMouseDown.Y));
+            }
+        }
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isMoving = false;//松开鼠标后移动程序关闭
+        }
+
+
+
+
+        /// <summary>
+        ///  选取歌曲文件
+        /// </summary>
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            ofd.ShowDialog();
+
+            string fileName = ofd.FileName;
+
+
+            //
+            axWindowsMediaPlayer1.URL = fileName;
+            //url后是文件地址
+            axWindowsMediaPlayer1.Ctlcontrols.stop ();
+        }
+
+
+        /// <summary>
+        /// 切换播放图片
+        /// 
+        /// </summary>
+        /// 
+
+        bool isPlaying = false;
+        private void pictureplay_Click(object sender, EventArgs e)
+        {
+            isPlaying = !isPlaying;
+            if (isPlaying)
+            {
+                pictureplay.BackgroundImage = Properties.Resources.pause_circle_o;
+
+                axWindowsMediaPlayer1.Ctlcontrols.play();
+            }
+            else
+            {
+                pictureplay.BackgroundImage = Properties.Resources.play_circle_o;//三角形
+
+                axWindowsMediaPlayer1.Ctlcontrols.pause();
+            }
+        }
+
+
+
+        /// <summary>
+        ///  载入歌词文件
+        /// </summary>
+
+        private void butLoadLyricc_Click(object sender, EventArgs e)
+        {
+            //打开文件，读取歌词
+            Encoding encode = Encoding.GetEncoding("GB2312");//适应中文码
+
+            //打开文件，读取歌词
+            FileStream fs = new FileStream("陈一发儿-童话镇.lrc", FileMode.Open);
+            StreamReader sr = new StreamReader(fs, encode);
+            string s = sr.ReadLine();
+            int xPos = 200;
+            int yPos = 50;
+
+            while (s != null)
+            {
+                s = sr.ReadLine();
+
+                if (s == "")
+                {
+                    continue;
+                }
+
+                Label lbl = new Label();
+                lbl.Font = new Font("微软雅黑", 12);
+                lbl.BackColor = Color.Transparent;
+                lbl.ForeColor = Color.White;
+                lbl.Location = new Point(xPos, yPos);
+                lbl.Width = 200;
+                lbl.Text = s;
+
+                this.Controls.Add(lbl);
+
+                yPos += 30;
+            }
+        }
+       
+    }
+}
